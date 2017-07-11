f610e96cb86a1c71144237c0dc5cd222b9cce298
Kaya Tatar <tatark@mit.edu>
2017-07-11 10:43:33 -0400

    use for loop to run different bins

--- doff.sh
+++ doff.sh
@@ -5,20 +5,47 @@ then
   exit 1
 fi
 
+# ./doff.sh 60 9999 30 0 1 0 0 0 DUMMYINPUT
+# ./doff.sh DUMMY DUMMY DUMMY 0 1 0 0 0 DUMMYINPUT
+runCmd="$HOME/code/scripts/myRun.sh"
 
-./ffgamma.exe $9 pbpbdata 0 20 $1 $2 $3 recoreco $4 $5 $6 $7 $8 &
-./ffgamma.exe $9 pbpbdata 20 60 $1 $2 $3 recoreco $4 $5 $6 $7 $8 &
-./ffgamma.exe $9 pbpbdata 60 100 $1 $2 $3 recoreco $4 $5 $6 $7 $8 &
-./ffgamma.exe $9 pbpbdata 100 200 $1 $2 $3 recoreco $4 $5 $6 $7 $8 &
+set -x
 
-wait
+inputPbPb="/mnt/hadoop/cms/store/user/tatar/GJT-out/skims/data_pbpb.root"
+inputPP="/mnt/hadoop/cms/store/user/tatar/GJT-out/skims/data_pp.root"
 
-./ffgamma.exe /export/d00/scratch/biran/photon-jet-track/pp-Data-skim-170406.root ppdata 0 20 $1 $2 $3 recoreco $4 $5 $6 $7 $8 &
-./ffgamma.exe /export/d00/scratch/biran/photon-jet-track/pp-Data-skim-170406.root ppdata 20 60 $1 $2 $3 recoreco $4 $5 $6 $7 $8 &
-./ffgamma.exe /export/d00/scratch/biran/photon-jet-track/pp-Data-skim-170406.root ppdata 60 100 $1 $2 $3 recoreco $4 $5 $6 $7 $8 &
-./ffgamma.exe /export/d00/scratch/biran/photon-jet-track/pp-Data-skim-170406.root ppdata 100 200 $1 $2 $3 recoreco $4 $5 $6 $7 $8 &
+centMins=(0  20  60 100);
+centMaxs=(20 60 100 200);
 
-wait
+phoPtMins=(60 80);
+phoPtMaxs=(9999 9999);
+jetPtMins=(30 40);
 
-hadd -f all_${1}_${2}_${3}_gammaxi${6}.root pbpbdata_pbpbdata_*_*.root ppdata_ppdata_*_*.root 
+indicesCent=${!centMins[*]}
+indicesPt=${!phoPtMins[*]}
+
+for iCent in $indicesCent
+do
+  for iPt in $indicesPt
+  do
+    centMin=${centMins[iCent]}
+    centMax=${centMaxs[iCent]}
+
+    phoPtMin=${phoPtMins[iPt]}
+    phoPtMax=${phoPtMaxs[iPt]}
+    jetPtMin=${jetPtMins[iPt]}
+
+    $runCmd ./ffgamma.exe $inputPbPb pbpbdata $centMin $centMax $phoPtMin $phoPtMax $jetPtMin recoreco $4 $5 $6 $7 $8 &> ffgamma_pbpbdata_${centMin}_${centMax}_${phoPtMin}_${phoPtMax}_${jetPtMin}_recoreco_${4}_${5}_${6}_${7}_${8}.log &
+    wait
+
+    $runCmd ./ffgamma.exe $inputPP ppdata $centMin $centMax $phoPtMin $phoPtMax $jetPtMin recoreco $4 $5 $6 $7 $8 &> ffgamma_ppdata_${centMin}_${centMax}_${phoPtMin}_${phoPtMax}_${jetPtMin}_recoreco_${4}_${5}_${6}_${7}_${8}.log &
+    wait
+
+    $runCmd hadd -f all_${phoPtMin}_${phoPtMax}_${jetPtMin}_gammaxi${6}.root pbpbdata_pbpbdata_*_*.root ppdata_ppdata_*_*.root &> hadd_all_${phoPtMin}_${phoPtMax}_${jetPtMin}_gammaxi${6}.log &
+    wait
+
+  done
+done
+
 rm pbpbdata_pbpbdata_*_*.root ppdata_ppdata_*_*.root 
+
