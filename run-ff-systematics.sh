eeb9b8459689afafd5698d80ebed8ff4435b6541
Kaya Tatar <tatark@mit.edu>
2017-08-08 15:10:23 -0400

    run the systematics variations for 30-100% and 0-30% as well. Do not make the calculation, only run the variation.

--- run-ff-systematics.sh
+++ run-ff-systematics.sh
@@ -58,6 +58,8 @@ do
     ./jetff.exe $SKIM $6 20 60 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}${SYSTEMATIC[SYS]} $SYS $kFF &
     ./jetff.exe $SKIM $6 60 100 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}${SYSTEMATIC[SYS]} $SYS $kFF &
     ./jetff.exe $SKIM $6 100 200 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}${SYSTEMATIC[SYS]} $SYS $kFF &
+    ./jetff.exe $SKIM $6 0 60 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}${SYSTEMATIC[SYS]} $SYS $kFF &
+    ./jetff.exe $SKIM $6 60 200 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}${SYSTEMATIC[SYS]} $SYS $kFF &
 done
 wait
 
@@ -69,10 +71,14 @@ wait
 ./jetff.exe $MCSKIM $MCSAMPLE 20 60 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}nominal_iso 0 $kFF &
 ./jetff.exe $MCSKIM $MCSAMPLE 60 100 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}nominal_iso 0 $kFF &
 ./jetff.exe $MCSKIM $MCSAMPLE 100 200 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}nominal_iso 0 $kFF &
+./jetff.exe $MCSKIM $MCSAMPLE 0 60 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}nominal_iso 0 $kFF &
+./jetff.exe $MCSKIM $MCSAMPLE 60 200 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}nominal_iso 0 $kFF &
 ./jetff.exe $MCSKIM $MCSAMPLE 0 20 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}iso 5 $kFF &
 ./jetff.exe $MCSKIM $MCSAMPLE 20 60 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}iso 5 $kFF &
 ./jetff.exe $MCSKIM $MCSAMPLE 60 100 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}iso 5 $kFF &
 ./jetff.exe $MCSKIM $MCSAMPLE 100 200 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}iso 5 $kFF &
+./jetff.exe $MCSKIM $MCSAMPLE 0 60 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}iso 5 $kFF &
+./jetff.exe $MCSKIM $MCSAMPLE 60 200 $1 $2 $3 $TYPE $4 $5 ${sysPrefix}iso 5 $kFF &
 wait
 
 hadd -f ${sysPrefix}nominal_iso_${MCSAMPLE}_${1}_${3}_gxi${5}_defnFF${kFF}_${TYPE}_ff.root ${sysPrefix}nominal_iso_${MCSAMPLE}_${TYPE}_${1}_${3}_${5}_${kFF}_*_*.root
@@ -96,62 +102,66 @@ do
     ./draw_ff.exe $6 ${sysPrefix}${SYSTEMATIC[SYS]}_${6}_${1}_${3}_gxi${5}_defnFF${kFF}_ff_merged.root ${sysPrefix}${SYSTEMATIC[SYS]}_${6}_${1}_${3}_gxi${5}_defnFF${kFF}_ff_final.root ${1} 0 ${TYPE}
 done
 
-SYSLIST=systematics_${1}_${3}_${5}_${6}.list
-if [ -f $SYSLIST ]; then
-    rm $SYSLIST
-fi
-touch $SYSLIST
+# SYSLIST=systematics_${1}_${3}_${5}_${6}.list
+# if [ -f $SYSLIST ]; then
+#     rm $SYSLIST
+# fi
+# touch $SYSLIST
 
-for SYS in 1 2 3 4 5 6 7 8 9 10
-do
-    echo -e "${sysPrefix}${SYSTEMATIC[SYS]}_${6}_${1}_${3}_gxi${5}_defnFF${kFF}_ff_final.root" >> $SYSLIST
-done
+# for SYS in 1 2 3 4 5 6 7 8 9 10
+# do
+#     echo -e "${sysPrefix}${SYSTEMATIC[SYS]}_${6}_${1}_${3}_gxi${5}_defnFF${kFF}_ff_final.root" >> $SYSLIST
+# done
 
-# placeholder for systematics yet to be implemented
-# none
+# # placeholder for systematics yet to be implemented
+# # none
 
-HISTLIST=hist_${1}_${3}_${5}_${6}.list
-if [ -f $HISTLIST ]; then
-    rm $HISTLIST
-fi
-touch $HISTLIST
-echo -e "hff_final_${6}_${TYPE}_0_20" >> $HISTLIST
-echo -e "hff_final_${6}_${TYPE}_20_60" >> $HISTLIST
-echo -e "hff_final_${6}_${TYPE}_60_100" >> $HISTLIST
-echo -e "hff_final_${6}_${TYPE}_100_200" >> $HISTLIST
+# HISTLIST=hist_${1}_${3}_${5}_${6}.list
+# if [ -f $HISTLIST ]; then
+#     rm $HISTLIST
+# fi
+# touch $HISTLIST
+# echo -e "hff_final_${6}_${TYPE}_0_20" >> $HISTLIST
+# echo -e "hff_final_${6}_${TYPE}_20_60" >> $HISTLIST
+# echo -e "hff_final_${6}_${TYPE}_60_100" >> $HISTLIST
+# echo -e "hff_final_${6}_${TYPE}_100_200" >> $HISTLIST
+# echo -e "hff_final_${6}_${TYPE}_0_60" >> $HISTLIST
+# echo -e "hff_final_${6}_${TYPE}_60_200" >> $HISTLIST
 
-cp $sysDir"/"data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root .
-./calc_systematics.exe $7 $SYSLIST $HISTLIST data_${1}_${3}_gxi${5}_defnFF${kFF}
-mv sys_hff_final_${6}_${TYPE}_*_*-data_${1}_${3}_gxi${5}_defnFF${kFF}.png $sysDir
-mv data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root $sysDir
+# cp $sysDir"/"data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root .
+# ./calc_systematics.exe $7 $SYSLIST $HISTLIST data_${1}_${3}_gxi${5}_defnFF${kFF}
+# mv sys_hff_final_${6}_${TYPE}_*_*-data_${1}_${3}_gxi${5}_defnFF${kFF}.png $sysDir
+# mv data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root $sysDir
 
-rm $SYSLIST
-rm $HISTLIST
+# rm $SYSLIST
+# rm $HISTLIST
 
-echo "running ratio systematics"
-SYSHISTLIST=syshist_${1}_${3}_${5}.list
-if [ -f $SYSHISTLIST ]; then
-  rm $SYSHISTLIST
-fi
-echo -e "0_20" >> $SYSHISTLIST
-echo -e "20_60" >> $SYSHISTLIST
-echo -e "60_100" >> $SYSHISTLIST
-echo -e "100_200" >> $SYSHISTLIST
+# echo "running ratio systematics"
+# SYSHISTLIST=syshist_${1}_${3}_${5}.list
+# if [ -f $SYSHISTLIST ]; then
+#   rm $SYSHISTLIST
+# fi
+# echo -e "0_20" >> $SYSHISTLIST
+# echo -e "20_60" >> $SYSHISTLIST
+# echo -e "60_100" >> $SYSHISTLIST
+# echo -e "100_200" >> $SYSHISTLIST
+# echo -e "0_60" >> $SYSHISTLIST
+# echo -e "60_200" >> $SYSHISTLIST
 
-SYSFILELIST=sysfile_${1}_${3}_${5}.list
-if [ -f $SYSFILELIST ]; then
-  rm $SYSFILELIST
-fi
-echo -e "data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root" >> $SYSFILELIST
-echo -e "data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root" >> $SYSFILELIST
+# SYSFILELIST=sysfile_${1}_${3}_${5}.list
+# if [ -f $SYSFILELIST ]; then
+#   rm $SYSFILELIST
+# fi
+# echo -e "data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root" >> $SYSFILELIST
+# echo -e "data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root" >> $SYSFILELIST
 
-cp $sysDir"/"data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root .
-./calc_ratio_systematics ff $SYSFILELIST $SYSHISTLIST data_${1}_${3}_gxi${5}_defnFF${kFF}
-mv sys_hff_final_*_*_*-data_${1}_${3}_gxi${5}_defnFF${kFF}.png $sysDir
-mv data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root $sysDir
+# cp $sysDir"/"data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root .
+# ./calc_ratio_systematics ff $SYSFILELIST $SYSHISTLIST data_${1}_${3}_gxi${5}_defnFF${kFF}
+# mv sys_hff_final_*_*_*-data_${1}_${3}_gxi${5}_defnFF${kFF}.png $sysDir
+# mv data_${1}_${3}_gxi${5}_defnFF${kFF}-systematics.root $sysDir
 
-rm $SYSHISTLIST
-rm $SYSFILELIST
+# rm $SYSHISTLIST
+# rm $SYSFILELIST
 
 
 
