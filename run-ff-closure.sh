5716389ccb3534003397d5297af9638e6f546459
Kaya Tatar <tatark@mit.edu>
2017-09-12 19:35:15 -0400

    update skim files with new ele rej and dphi cuts. used the version here : https://github.com/ttrk/gammajettrack/commit/8a3d14eb044d4a80e7d14289b352b1eb5676e169

--- run-ff-closure.sh
+++ run-ff-closure.sh
@@ -17,9 +17,9 @@ echo "label    = $8"
 echo "types    = $9"
 
 if [ $7 = "pbpbmc" ]; then
-    SKIM="/export/d00/scratch/biran/photon-jet-track/PbPb-MC-skim-170824.root"
+    SKIM="/export/d00/scratch/biran/photon-jet-track/PbPb-MC-skim-170911.root"
 elif [ $7 = "ppmc" ]; then
-    SKIM="/export/d00/scratch/biran/photon-jet-track/pp-MC-skim-170824.root"
+    SKIM="/export/d00/scratch/tatar/GJT-out/pp-MC-skim-170911.root"
 else
     echo "invalid sample"
     exit 1
