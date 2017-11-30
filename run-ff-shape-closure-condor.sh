97ee135ea21bd3d056445ca786f38807d6dc27ab
Kaya Tatar <tatark@mit.edu>
2017-11-30 13:25:08 -0500

    add shell script to create condor jobs which run jetffshape.C for mc closure. This is a version of run-ff-shape-closure.sh where each call to jetffshape.C becomes a condor job

---
+++ run-ff-shape-closure-condor.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+if [ $# -lt 9 ]; then
+  echo "Usage: ./run-ff-shape-closure-condor.sh [phoetmin] [phoetmax] [jetptmin] [trkptmin] [gammaxi] [obs] [sample] [label] [types...]"
+  echo "Example: ./run-ff-shape-closure-condor.sh 80 1000 40 1 0 1 pbpbmc ffclosure sgengen sgenreco recogen recoreco &> logFile &"
+  exit 1
+fi
+
+# transfer the standard out to a log file and then grep the condor submission commands via
+# grep "condor_submit" <logfile>
+
+phoetMin=$1
+phoetMax=$2
+jetptMin=$3
+trkptMin=$4
+gammaxi=$5
+obs=$6
+sample=$7
+label=$8
+recogenLevels=${@:9}
+
+#outputDir="/mnt/hadoop/cms/store/user/"$USER"/GJT-out/results/closure/"
+outputDir="/mnt/hadoop/cms/store/user/katatar/GJT-out/results/closure/"
+
+echo "phoetMin = $phoetMin"
+echo "phoetMax = $phoetMax"
+echo "jetptMin = $jetptMin"
+echo "trkptMin = $trkptMin"
+echo "gammaxi  = $gammaxi"
+echo "obs      = $obs"
+echo "sample   = $sample"
+echo "label    = $label"
+echo "recogenLevels = $recogenLevels"
+echo "outputDir = $outputDir"
+
+if [ $sample = "pbpbmc" ]; then
+    SKIM="/mnt/hadoop/cms/store/user/tatar/GJT-out/PbPb-MC-skim-170911.root"
+elif [ $sample = "ppmc" ]; then
+    SKIM="/mnt/hadoop/cms/store/user/tatar/GJT-out/pp-MC-skim-170911.root"
+else
+    echo "invalid sample"
+    exit 1
+fi
+
+echo "compiling macros..."
+g++ jetffshape.C $(root-config --cflags --libs) -Werror -Wall -O2 -o jetffshape.exe || exit 1
+
+#set -x
+
+echo prepare condor jobs for closure histograms
+for rgLevel in $recogenLevels; do
+  if [ ! -f ${label}_${sample}_${phoetMin}_${phoetMax}_gxi${gammaxi}_obs${obs}_${rgLevel}_ffjs.root ]; then
+    hiBinMins=(0  20 60  100)
+    hiBinMaxs=(20 60 100 200)
+    for i1 in ${!hiBinMins[*]}
+    do
+      hiBinMin=${hiBinMins[i1]}
+      hiBinMax=${hiBinMaxs[i1]}
+     ./condor/condorSubmit_jetffshape.sh $SKIM $sample $hiBinMin $hiBinMax $phoetMin $phoetMax $jetptMin $trkptMin $gammaxi jsclosure 0 $obs $rgLevel $outputDir
+    done
+  fi
+done
+wait
+
+#for rgLevel in $recogenLevels; do
+#  if [ ! -f ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_${rgLevel}_ffjs.root ]; then
+#    hadd -f ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_${rgLevel}_ffjs.root ${label}_${sample}_${rgLevel}_${phoetMin}_${jetptMin}_${gammaxi}_${obs}_*_*.root
+#    rm ${label}_${sample}_${rgLevel}_${phoetMin}_${jetptMin}_${gammaxi}_${obs}_*_*.root
+#  fi
+#done
+
+#./run-ff-shape-plot.sh $@
+
+#outDir="/export/d00/scratch/"$USER"/GJT-out/results/closure/"
+#mkdir -p $outDir
+#mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_*_ffjs.root $outDir
+#mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_ffjs_merged.root $outDir
+#mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_ffjs_final.root $outDir
