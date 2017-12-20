c0bfec1120e7fd570b7fb1f8fa464b285ab73869
Kaya Tatar <tatark@mit.edu>
2017-12-19 23:28:13 -0500

    output directory becomes an argument

--- run-ff-shape-closure.sh
+++ run-ff-shape-closure.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-if [ $# -lt 9 ]; then
-  echo "Usage: ./run-ff-shape-closure.sh [phoetmin] [phoetmax] [jetptmin] [trkptmin] [gammaxi] [obs] [sample] [label] [types...]"
-  echo "Example: ./run-ff-shape-closure.sh 80 1000 40 1 0 1 pbpbmc ffclosure sgengen sgenreco recogen recoreco"
+if [ $# -lt 10 ]; then
+  echo "Usage: ./run-ff-shape-closure.sh [outputDir] [phoetmin] [phoetmax] [jetptmin] [trkptmin] [gammaxi] [obs] [sample] [label] [types...]"
+  echo "Example: ./run-ff-shape-closure.sh /export/d00/scratch/$USER/GJT-out/results/closure/ 80 1000 40 1 0 1 pbpbmc ffclosure sgengen sgenreco recogen recoreco"
   exit 1
 fi
 
-phoetMin=$1
-phoetMax=$2
-jetptMin=$3
-trkptMin=$4
-gammaxi=$5
-obs=$6
-sample=$7
-label=$8
-recogenLevels=${@:9}
+outputDir=$1
+phoetMin=$2
+phoetMax=$3
+jetptMin=$4
+trkptMin=$5
+gammaxi=$6
+obs=$7
+sample=$8
+label=$9
+recogenLevels=${@:10}
 
-echo "phoetMin = $phoetMin"
-echo "phoetMax = $phoetMax"
-echo "jetptMin = $jetptMin"
-echo "trkptMin = $trkptMin"
-echo "gammaxi  = $gammaxi"
-echo "obs      = $obs"
-echo "sample   = $sample"
-echo "label    = $label"
+echo "outputDir = $outputDir"
+echo "phoetMin  = $phoetMin"
+echo "phoetMax  = $phoetMax"
+echo "jetptMin  = $jetptMin"
+echo "trkptMin  = $trkptMin"
+echo "gammaxi   = $gammaxi"
+echo "obs       = $obs"
+echo "sample    = $sample"
+echo "label     = $label"
 echo "recogenLevels = $recogenLevels"
 
 if [ $sample = "pbpbmc" ]; then
@@ -38,6 +40,8 @@ fi
 echo "compiling macros..."
 g++ jetffshape.C $(root-config --cflags --libs) -Werror -Wall -O2 -o jetffshape.exe || exit 1
 
+mkdir -p $outputDir
+
 set -x
 
 echo running closure histograms
@@ -49,25 +53,25 @@ for rgLevel in $recogenLevels; do
     do
       hiBinMin=${hiBinMins[i1]}
       hiBinMax=${hiBinMaxs[i1]}
-     ./jetffshape.exe $SKIM $sample $hiBinMin $hiBinMax $phoetMin $phoetMax $jetptMin $rgLevel  $trkptMin $gammaxi $label 0 $obs &
+     ./jetffshape.exe $SKIM $sample $hiBinMin $hiBinMax $phoetMin $phoetMax $jetptMin $rgLevel $trkptMin $gammaxi ${outputDir}/$label 0 $obs &
     done
   fi
 done
 wait
 
 for rgLevel in $recogenLevels; do
-  outputFile=${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_${rgLevel}_ffjs.root
-  inputPrefix=${label}_${sample}_${rgLevel}_${phoetMin}_${jetptMin}_${gammaxi}_${obs}
+  outputFile=${outputDir}/${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_${rgLevel}_ffjs.root
+  inputPrefix=${outputDir}/${label}_${sample}_${rgLevel}_${phoetMin}_${jetptMin}_${gammaxi}_${obs}
   if [ ! -f $outputFile ]; then
     hadd -f $outputFile ${inputPrefix}_*_*.root
     rm ${inputPrefix}_*_*.root
   fi
 done
 
-#./run-ff-shape-plot.sh $@
+outputFilesMerged=${outputDir}/${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_ffjs_merged.root
+hadd -f $outputFilesMerged ${outputDir}/${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_*_ffjs.root
 
-outDir="/export/d00/scratch/"$USER"/GJT-out/results/closure/"
-mkdir -p $outDir
-mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_*_ffjs.root $outDir
-mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_ffjs_merged.root $outDir
-mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_ffjs_final.root $outDir
+#./run-ff-shape-plot.sh $@
+#mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_*_ffjs.root $outDir
+#mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_ffjs_merged.root $outDir
+#mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_ffjs_final.root $outDir
