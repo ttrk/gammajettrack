5e3950531c18957d9a07579e642fe7d4877b4ae8
Kaya Tatar <tatark@mit.edu>
2018-01-21 22:23:29 -0500

    add example script calls

--- run-save-dphidetarefrecoJet-ptBin.sh
+++ run-save-dphidetarefrecoJet-ptBin.sh
@@ -2,6 +2,8 @@
 
 if [ $# -lt 3 ]; then
   echo "Usage: ./save-dphidetarefrecoJet-ptBin.sh [inputFile] [outputFile] [sample]"
+  echo "Example: ./run-save-dphidetarefrecoJet-ptBin.sh /export/d00/scratch/${USER}/GJT-out/results/closure/jsclosure_ppmc_60_30_gxi0_obs2_ffjs_merged.root pp-weights.root ppmc"
+  echo "Example: ./run-save-dphidetarefrecoJet-ptBin.sh /export/d00/scratch/${USER}/GJT-out/results/closure/jsclosure_pbpbmc_60_30_gxi0_obs2_ffjs_merged.root PbPb-weights.root pbpbmc"
   exit 1
 fi
 
