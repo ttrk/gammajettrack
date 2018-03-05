242312f670e5371f8bbde2aa53fe5ddddd42f835
Kaya Tatar <tatark@mit.edu>
2018-03-05 16:10:27 -0500

    assign arguments to bash variables

--- run-ff-shape-data.sh
+++ run-ff-shape-data.sh
@@ -1,92 +1,81 @@
 #!/bin/bash
 
-if [ $# -ne 7 ]; then
-  echo "Usage: ./run-ff-shape-data.sh [phoetmin] [phoetmax] [jetptmin] [trkptmin] [gammaxi] [defnFF] [coll]"
-  echo "       [coll] - pbpb, pp, both"
-  echo "Example: ./run-ff-shape-data.sh 80 1000 40 1 0 0 2"
+if [ $# -lt 10 ]; then
+  echo "Usage: ./run-ff-shape-data.sh [outputDir] [phoetmin] [phoetmax] [jetptmin] [trkptmin] [gammaxi] [obs] [sample] [label] [types...]"
+  echo "Example: ./run-ff-shape-data.sh /export/d00/scratch/$USER/GJT-out/results/closure/ 80 1000 40 1 0 1 pbpbdata jsdata sgengen sgenreco recogen recoreco"
   exit 1
 fi
 
-echo "phoetmin = $1"
-echo "phoetmax = $2"
-echo "jetptmin = $3"
-echo "trkptmin = $4"
-echo "gammaxi  = $5"
-echo "defnFF   = $6"
-echo "coll     = $7"
+outputDir=$1
+phoetMin=$2
+phoetMax=$3
+jetptMin=$4
+trkptMin=$5
+gammaxi=$6
+obs=$7
+sample=$8
+label=$9
+recogenLevels=${@:10}
 
+echo "outputDir = $outputDir"
+echo "phoetMin  = $phoetMin"
+echo "phoetMax  = $phoetMax"
+echo "jetptMin  = $jetptMin"
+echo "trkptMin  = $trkptMin"
+echo "gammaxi   = $gammaxi"
+echo "obs       = $obs"
+echo "sample    = $sample"
+echo "label     = $label"
+echo "recogenLevels = $recogenLevels"
+
+if [ $sample = "pbpbdata" ]; then
+    SKIM="/export/d00/scratch/tatar/GJT-out/PbPb-Data-skim-170911.root"
+elif [ $sample = "ppdata" ]; then
+    SKIM="/export/d00/scratch/tatar/GJT-out/pp-Data-skim-170911.root"
+else
+    echo "invalid sample"
+    exit 1
+fi
+
+echo "compiling macros..."
 g++ jetffshape.C $(root-config --cflags --libs) -Werror -Wall -O2 -o jetffshape.exe || exit 1
-echo "g++ jetffshape.C $(root-config --cflags --libs) -Werror -Wall -O2 -o jetffshape.exe || exit 1"
 
-PBPBSKIM="/export/d00/scratch/tatar/GJT-out/PbPb-Data-skim-170911.root"
-PPSKIM="/export/d00/scratch/tatar/GJT-out/pp-Data-skim-170911.root"
+mkdir -p $outputDir
 
-outDir="/export/d00/scratch/"$USER"/GJT-out/results/"
-mkdir -p $outDir
-echo "outDir : $outDir"
+set -x
 
-systematics=13
-set +x
+echo running closure histograms
+for rgLevel in $recogenLevels; do
+#  if [ ! -f ${label}_${sample}_${phoetMin}_${phoetMax}_gxi${gammaxi}_obs${obs}_${rgLevel}_ffjs.root ]; then
+    hiBinMins=(0  20 60  100 0  60)
+    hiBinMaxs=(20 60 100 200 60 200)
+    if [ $sample = "ppdata" ] && [ $rgLevel = "recoreco" ]; then
+      hiBinMins=(100)
+      hiBinMaxs=(200)
+    fi
+    for i1 in ${!hiBinMins[*]}
+    do
+      hiBinMin=${hiBinMins[i1]}
+      hiBinMax=${hiBinMaxs[i1]}
+     ./jetffshape.exe $SKIM $sample $hiBinMin $hiBinMax $phoetMin $phoetMax $jetptMin $rgLevel $trkptMin $gammaxi ${outputDir}/$label 0 $obs &
+    done
+#  fi
+done
+wait
 
-if [[ $7 == "pbpb" ]]; then
-    echo "running on pbpb data"
-    echo "PBPBSKIM : $PBPBSKIM"
-    set -x
-    outPrefix=$outDir"data"
-    ./jetffshape.exe $PBPBSKIM pbpbdata 0 20 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PBPBSKIM pbpbdata 20 60 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PBPBSKIM pbpbdata 60 100 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PBPBSKIM pbpbdata 100 200 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PBPBSKIM pbpbdata 0 60 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PBPBSKIM pbpbdata 60 200 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    wait
+for rgLevel in $recogenLevels; do
+  outputFile=${outputDir}/${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_${rgLevel}_ffjs.root
+  inputPrefix=${outputDir}/${label}_${sample}_${rgLevel}_${phoetMin}_${jetptMin}_${gammaxi}_${obs}
+#  if [ ! -f $outputFile ]; then
+  hadd -f $outputFile ${inputPrefix}_*_*.root
+  rm ${inputPrefix}_*_*.root
+#  fi
+done
 
-    hadd -f ${outPrefix}_pbpbdata_${1}_${3}_gxi${5}_defnFF${6}_recoreco_ff.root ${outPrefix}_pbpbdata_recoreco_${1}_${3}_${5}_${6}_*_*.root
-    rm ${outPrefix}_pbpbdata_recoreco_${1}_${3}_${5}_${6}_*_*.root
+outputFilesMerged=${outputDir}/${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_ffjs_merged.root
+hadd -f $outputFilesMerged ${outputDir}/${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_*_ffjs.root
 
-    ./run-ff-shape-plot.sh $1 $2 $3 $4 $5 $6 pbpbdata data recoreco
-fi
-
-if [[ $7 == "pp" ]]; then
-    echo "running on pp data"
-    echo "PPSKIM : $PPSKIM"
-    set -x
-    outPrefix=$outDir"data"
-    ./jetffshape.exe $PPSKIM ppdata 0 20 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdata 20 60 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdata 60 100 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdata 100 200 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdata 0 60 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdata 60 200 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-
-    ./jetffshape.exe $PPSKIM ppdatareweight 0 20 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdatareweight 20 60 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdatareweight 60 100 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdatareweight 100 200 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdatareweight 0 60 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdatareweight 60 200 $1 $2 $3 srecoreco $4 $5 $outPrefix $systematics $6 &
-
-    ./jetffshape.exe $PPSKIM ppdata 0 20 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdata 20 60 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdata 60 100 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdata 100 200 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdata 0 60 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    ./jetffshape.exe $PPSKIM ppdata 60 200 $1 $2 $3 recoreco $4 $5 $outPrefix $systematics $6 &
-    wait
-
-    hadd -f ${outPrefix}_ppdata_${1}_${3}_gxi${5}_defnFF${6}_srecoreco_ff.root ${outPrefix}_ppdata_srecoreco_${1}_${3}_${5}_${6}_*_*.root
-    hadd -f ${outPrefix}_ppdatareweight_${1}_${3}_gxi${5}_defnFF${6}_srecoreco_ff.root ${outPrefix}_ppdatareweight_srecoreco_${1}_${3}_${5}_${6}_*_*.root
-    hadd -f ${outPrefix}_ppdata_${1}_${3}_gxi${5}_defnFF${6}_recoreco_ff.root ${outPrefix}_ppdata_recoreco_${1}_${3}_${5}_${6}_*_*.root
-    rm ${outPrefix}_ppdata_srecoreco_${1}_${3}_${5}_${6}_*_*.root
-    rm ${outPrefix}_ppdatareweight_srecoreco_${1}_${3}_${5}_${6}_*_*.root
-    rm ${outPrefix}_ppdata_recoreco_${1}_${3}_${5}_${6}_*_*.root
-
-    ./run-ff-shape-plot.sh $1 $2 $3 $4 $5 $6 ppdata data 0
-fi
-
-set +x
-if [[ $7 == "both" ]]; then
-    echo "running plotting for pbpb and pp"
-    set -x
-    ./run-ff-shape-plot.sh $1 $2 $3 $4 $5 $6 data data data
-fi
+#./run-ff-shape-plot.sh $@
+#mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_*_ffjs.root $outDir
+#mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_ffjs_merged.root $outDir
+#mv ${label}_${sample}_${phoetMin}_${jetptMin}_gxi${gammaxi}_obs${obs}_ffjs_final.root $outDir
