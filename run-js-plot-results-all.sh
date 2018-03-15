35fe6ac2380d48637a312aef86856e67589195eb
Kaya Tatar <tatark@mit.edu>
2018-03-15 15:28:52 -0400

    followup : add plots the systematics are split into "photon related unc.", "jet related unc." and "remaining unc." categories

--- run-js-plot-results-all.sh
+++ run-js-plot-results-all.sh
@@ -22,6 +22,21 @@ inputDirClosure="/export/d00/scratch/"$USER"/GJT-out/results/closure/"
 ./run-js-plot-results.sh 60 9999 30 1 0 2 ppdata sysalltotpercnt $inputDir DUMMY
 ./run-js-plot-results.sh 60 9999 30 1 0 2 ratio sysalltotpercnt $inputDir DUMMY
 
+## sys var plots - All photon related variation together and total systematics in percentage
+./run-js-plot-results.sh 60 9999 30 1 0 2 pbpbdata sysallpercnt_phosys $inputDir DUMMY
+./run-js-plot-results.sh 60 9999 30 1 0 2 ppdata sysallpercnt_phosys $inputDir DUMMY
+./run-js-plot-results.sh 60 9999 30 1 0 2 ratio sysallpercnt_phosys $inputDir DUMMY
+
+## sys var plots - All jet related variation together and total systematics in percentage
+./run-js-plot-results.sh 60 9999 30 1 0 2 pbpbdata sysallpercnt_jetsys $inputDir DUMMY
+./run-js-plot-results.sh 60 9999 30 1 0 2 ppdata sysallpercnt_jetsys $inputDir DUMMY
+./run-js-plot-results.sh 60 9999 30 1 0 2 ratio sysallpercnt_jetsys $inputDir DUMMY
+
+## sys var plots - All remaining variation together and total systematics in percentage
+./run-js-plot-results.sh 60 9999 30 1 0 2 pbpbdata sysallpercnt_othersys $inputDir DUMMY
+./run-js-plot-results.sh 60 9999 30 1 0 2 ppdata sysallpercnt_othersys $inputDir DUMMY
+./run-js-plot-results.sh 60 9999 30 1 0 2 ratio sysallpercnt_othersys $inputDir DUMMY
+
 ## closure plots
 ##./run-js-plot-results.sh 60 9999 30 1 0 2 pbpbmc closure $inputDirClosure DUMMY
 ##./run-js-plot-results.sh 60 9999 30 1 0 2 ppmc closure $inputDirClosure DUMMY
