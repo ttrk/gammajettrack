afee9ddfaff3c8740a1461184487dc7e9d73f77b
Kaya Tatar <tatark@mit.edu>
2018-07-25 15:56:15 -0400

    further "do not plot 80 40 results"

--- run-ff-plot-results-all.sh
+++ run-ff-plot-results-all.sh
@@ -5,8 +5,8 @@ inputDirClosure="/export/d00/scratch/"$USER"/GJT-out/results/closure/"
 
 ./run-ff-plot-results.sh 60 9999 30 1 0 1 pbpbdata data $inputDir recoreco
 ./run-ff-plot-results.sh 60 9999 30 1 1 1 pbpbdata data $inputDir recoreco
-./run-ff-plot-results.sh 80 9999 40 1 0 1 pbpbdata data $inputDir recoreco
-./run-ff-plot-results.sh 80 9999 40 1 1 1 pbpbdata data $inputDir recoreco
+#./run-ff-plot-results.sh 80 9999 40 1 0 1 pbpbdata data $inputDir recoreco
+#./run-ff-plot-results.sh 80 9999 40 1 1 1 pbpbdata data $inputDir recoreco
 
 ## sys var plots
 ./run-ff-plot-results.sh 60 9999 30 1 0 1 pbpbdata sysvar $inputDir recoreco
